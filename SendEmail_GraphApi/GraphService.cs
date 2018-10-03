5efc3cc06fe94bce93efff97e368d6a33f39c1ce
Srinivas Ch <srinivas.ch@ensurity.com>
2018-10-03 12:30:11 +0530

    GraphApi code

---
+++ SendEmail_GraphApi/GraphService.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Resources;
+using System.IO;
+using Microsoft.Graph;
+
+namespace SendEmail_GraphApi
+{
+
+    // This sample shows how to:
+    //    - Get the current user's email address
+    //    - Get the current user's profile photo
+    //    - Attach the photo as a file attachment to an email message
+    //    - Upload the photo to the user's root drive
+    //    - Get a sharing link for the file and add it to the message
+    //    - Send the email
+    public class GraphService
+    {
+        // Send an email message from the current user.
+        public async Task<string> SendEmail(string accessToken, MessageRequest email)
+        {
+            string endpoint = "https://graph.microsoft.com/v1.0/me/sendMail";
+
+            using (var client = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(HttpMethod.Post, endpoint))
+                {
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                    request.Content = new StringContent(JsonConvert.SerializeObject(email), Encoding.UTF8, "application/json");
+                    using (var response = await client.SendAsync(request))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            return Resource.Graph_SendMail_Success_Result;
+                        }
+                        return response.ReasonPhrase;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SendEmail_GraphApi/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmail_GraphApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program pp = new Program();
+            pp.SendEmail();
+        }
+
+        public async Task SendEmail()
+        {
+            // Arrange.
+            GraphService graphService = new GraphService();
+            string subject = "Test email from ASP.NET 4.6 Connect sample";
+            string bodyContent = "<html><body>The body of the test email.</body></html>";
+            List<Recipient> recipientList = new List<Recipient>();
+            recipientList.Add(new Recipient
+            {
+                EmailAddress = new EmailAddress
+                {
+                    Address = "srinivas.ch@ensurity.com"
+                }
+            });
+            Message message = new Message
+            {
+                Body = new ItemBody
+                {
+                    Content = bodyContent,
+                    ContentType = BodyType.Html,
+                },
+                Subject = subject,
+                ToRecipients = recipientList
+            };
+
+            // Act
+            Task task = graphService.SendEmail(client, message);
+
+            // Assert
+            Task.WaitAll(task);
+        }
+    }
+}
